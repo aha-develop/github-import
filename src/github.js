01b2c7a252f375e953ecb35bc8ba6e627bb6c7dd
Justin Weiss <justin@justinweiss.com>
2021-03-15 16:42:20 -0700

    Add repo autocomplete

--- src/github.js
+++ src/github.js
@@ -18,3 +18,97 @@ export async function withGitHubApi(callback) {
     }
   );
 }
+
+export async function autocompleteRepo(query) {
+  let results = [];
+  if (!query || query.length < 3) return results;
+
+  const [owner, repo] = query.split("/");
+  if (repo === undefined) {
+    await withGitHubApi(async (api) => {
+      const { search } = await api(`
+   {
+      search(query:"${owner}", type: USER, first:10 ) {
+        nodes {
+          __typename
+          ... on User {
+            login
+          }
+          ... on Organization {
+            login
+          }
+        }
+      }
+    }
+`);
+
+      results = search.nodes
+        .filter((result) => result.login)
+        .map((result) => ({
+          value: result.login + "/",
+        }));
+    });
+  } else {
+    await withGitHubApi(async (api) => {
+      const { search } = await api(`
+   {
+      search(query:"user:${owner} ${repo}", type: REPOSITORY, first:10 ) {
+        nodes {
+          __typename
+          ... on Repository {
+            nameWithOwner
+          }
+        }
+      }
+    }
+`);
+
+      results = search.nodes.map((result) => ({
+        value: result.nameWithOwner,
+      }));
+    });
+  }
+
+  return results;
+}
+
+export async function findIssues(repoWithOwner, cursor) {
+  let records = [];
+  let nextPage = null;
+  if (!repoWithOwner) return { records, nextPage };
+
+  const [owner, repo] = repoWithOwner.split("/");
+  if (!repo) return { records, nextPage };
+
+  await withGitHubApi(async (api) => {
+    const { search } = await api(`
+   {
+      search(query:"type:issue repo:\\"${repoWithOwner}\\"", type: ISSUE, first:20 ) {
+        nodes {
+          __typename
+          ... on Issue {
+            id
+            number
+            title
+            url
+            state
+          }
+        }
+        pageInfo {
+          endCursor
+        }
+      }
+    }
+`);
+
+    records = search.nodes.map((result) => ({
+      id: result.id,
+      identifier: "#" + result.number,
+      name: result.title,
+      url: result.url,
+    }));
+    nextPage = search.pageInfo.endCursor;
+  });
+
+  return { records, nextPage };
+}
--- src/import.js
+++ src/import.js
@@ -1,4 +1,4 @@
-import { withGitHubApi } from "./github";
+import { withGitHubApi, autocompleteRepo, findIssues } from "./github";
 
 // Return a list of the possible filters.
 aha.on(
@@ -17,8 +17,13 @@ aha.on(
 // For a particular filter, when it is dropped-down, provide a list of the possible values.
 aha.on(
   { import: "aha-develop.github-import.issues", action: "filterValues" },
-  async (filter, filters) => {
-    return [];
+  async ({ filter, filters }) => {
+    let values = [];
+    switch (filter) {
+      case "repo":
+        values = await autocompleteRepo(filters.repo);
+    }
+    return values;
   }
 );
 
@@ -28,70 +33,24 @@ aha.on(
     import: "aha-develop.github-import.issues",
     action: "listCandidates",
   },
-  async (filters, page) => {
-    console.log("filters", filters);
-
-    const repoRegExp = new RegExp("^https://github.com/([^/]+)/([^/]+)");
-    const match = repoRegExp.exec(filters.repo);
-    let results = [];
-
-    if (match) {
-      const repo = `${match[1]}/${match[2]}`;
-      console.log("repo", repo);
-
-      await withGitHubApi(async (api) => {
-        const { search } = await api(`
-   {
-      search(query:"type:issue repo:\\"${repo}\\"", type: ISSUE, first:20 ) {
-        nodes {
-          __typename
-          ... on Issue {
-            id
-            number
-            title
-            url
-            state
-          }
-        }
-      }
-    }
-`);
-
-        results = search.nodes.map((result) => ({
-          id: result.id,
-          identifier: "#" + result.number,
-          name: result.title,
-          url: result.url,
-        }));
-      });
-    } else {
-      console.log("no match");
-    }
-
-    return results;
+  async ({ filters, nextPage }) => {
+    return findIssues(filters.repo, nextPage);
   }
 );
 
 // Render a single record.
 aha.on(
   { import: "aha-develop.github-import.issues", action: "renderRecord" },
-  (record, element) => {
-    return `${record.identifier}<br /><a href="${record.url}">${record.name}</a>`;
+  ({ record, element }) => {
+    return `${record.identifier}<br /><a href="${record.url}" target="_blank" rel="noopener">${record.name}</a>`;
   }
 );
 
 // Prepare a single record for import.
 aha.on(
   { import: "aha-develop.github-import.issues", action: "importRecord" },
-  async (record, { teamId }) => {
-    // Add `record` to Aha!, potentially making API calls to the other system to fetch more information as necessary.
-    const feature = new aha.models.Feature({
-      name: record.name,
-      team: { id: teamId },
-    });
-
-    await feature.save();
-
-    return feature;
+  async ({ importRecord, ahaRecord }) => {
+    ahaRecord.name = "[GitHub] " + ahaRecord.name;
+    await ahaRecord.save();
   }
 );
